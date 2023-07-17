1e70bca18f27de01fa521f9e919fff8ffaf31c10
Solutions:Dor Oktan <doktan@bigid.com>
2023-07-17 17:02:13 +0300

    Modified it to run as a script and not in command line

--- generate_data.sh
+++ generate_data.sh
@@ -1,4 +1,4 @@
-do \
-curl "https://api.mockaroo.com/api/8c9b0990?count=5000&key=4eca2850" >> $1.csv; \
-echo "Iteration number $i"; \
+do
+curl "https://api.mockaroo.com/api/8c9b0990?count=5000&key=4eca2850" >> $1.csv 
+echo "Iteration number $i" 
 done
